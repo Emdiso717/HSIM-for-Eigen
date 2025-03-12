188e208a8782af4c77575dcefa6ac48ae494eea4
QinJizhou <897482672@qq.com>
2025-03-12 16:07:06 +0800

    fix

--- build-debug/CPM_modules/Findspectra.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/emdiso/c/HSIM/HSIM-for-Eigen/cmake/CPM.cmake")
-CPMAddPackage("NAME;spectra;GIT_REPOSITORY;https://github.com/yixuan/spectra.git;GIT_TAG;v1.0.1;PATCH_COMMAND;git;restore;.;&&;git;apply;/home/emdiso/c/HSIM/HSIM-for-Eigen/cmake/spectra_fix_eigen.patch")
-set(spectra_FOUND TRUE)
